307b4db87e002a38f9b933e40d2e190f64db8aa1
FerMeza <30847755+FerMeza@users.noreply.github.com>
2025-04-15 17:20:39 -0600

    feat: clean input function with tests

---
+++ repl_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "   hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf("actual input: %v is different from expected: %v", actual, c.expected)
+		}
+	}
+}
