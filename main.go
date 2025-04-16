d645ebb1366b7d0aeb12ef2b6dbe687194a94ff6
FerMeza <30847755+FerMeza@users.noreply.github.com>
2025-04-15 18:14:35 -0600

    feat: exit & help commands

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		moreTokens := scanner.Scan()
-		if !moreTokens {
-			err := scanner.Err()
-			if err != nil {
-				fmt.Printf("There was an error on input: %v", err)
-				return
-			}
-			fmt.Printf("Goodbye!")
-			return
-		}
-		userInput := scanner.Text()
-		cleanInput := cleanInput(userInput)
-		if len(cleanInput) == 0 {
-			fmt.Println("There is no command!")
-		} else {
-			fmt.Printf("Your command was: %s\n", cleanInput[0])
-		}
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,9 +1,90 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		moreTokens := scanner.Scan()
+		if !moreTokens {
+			err := scanner.Err()
+			if err != nil {
+				fmt.Printf("There was an error on input: %v", err)
+				return
+			}
+			fmt.Printf("Goodbye!")
+			return
+		}
+		userInput := scanner.Text()
+		cleanInput := cleanInput(userInput)
+		if len(cleanInput) == 0 {
+			continue
+		}
+
+		if len(cleanInput) >= 0 {
+			command, ok := commands[cleanInput[0]]
+			if !ok {
+				fmt.Println("Unknown command")
+				continue
+			} else {
+				err := command.callback()
+				if err != nil {
+					fmt.Printf("error executing command: %v", err)
+				}
+			}
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
 	words := strings.Fields(lowerText)
 	return words
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+	for _, val := range commands {
+		fmt.Printf("%s: %s\n", val.name, val.description)
+	}
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func init() {
+	commands["help"] = struct {
+		name        string
+		description string
+		callback    func() error
+	}{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
 }
